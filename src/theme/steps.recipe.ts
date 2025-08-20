8abf1d4a792dd0099d971003999779144b62a3d8
Raphael Vasseur <raphael.vasseur@loungeup.com>
2025-08-20 09:43:09 +0200

    remove unused comments

--- src/theme/steps.recipe.ts
+++ src/theme/steps.recipe.ts
@@ -3,47 +3,6 @@ import { stepsAnatomy } from "@chakra-ui/react/anatomy"
 
 export const stepsSlotRecipe = defineSlotRecipe({
   slots: stepsAnatomy.keys(),
-  // base: {
-  //   indicator: {
-  //     display: 'flex',
-  //     justifyContent: 'center',
-  //     alignItems: 'center',
-  //     flexShrink: '0',
-  //     borderRadius: 'full',
-  //     fontWeight: 'medium',
-  //     width: 'var(--steps-size)',
-  //     height: 'var(--steps-size)',
-  //     _icon: {
-  //       flexShrink: '0',
-  //       width: 'var(--steps-icon-size)',
-  //       height: 'var(--steps-icon-size)',
-  //     },
-  //   },
-  //   item: {
-  //     position: 'relative',
-  //     display: 'flex',
-  //     gap: '3',
-  //     flex: '1 0 0',
-  //     '&:last-of-type': {
-  //       flex: 'initial',
-  //       '& [data-part=separator]': {
-  //         display: 'none',
-  //       },
-  //     },
-  //   },
-  //   trigger: {
-  //     display: 'flex',
-  //     alignItems: 'center',
-  //     gap: '3',
-  //     textAlign: 'start',
-  //     focusVisibleRing: 'outside',
-  //     borderRadius: 'l2',
-  //   },
-  //   content: {
-  //     focusVisibleRing: 'outside',
-  //   },
-  // },
-
   variants: {
     variant: {
       rsi: {
